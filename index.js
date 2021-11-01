7e1117bb3833ee9044cebe952a50f4d70706ca46
Kunal <89037807+Kk015@users.noreply.github.com>
2021-11-01 20:36:48 +0530

    Corrected Audio Files
    Corrected Audio Files

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ function nextSequence() {
   animatePress(randomchosencolor);
 }
 function playSound(name) {
-  var audio = new Audio("sounds/" + name + ".mp3");
+  var audio = new Audio( name + ".mp3");
   audio.play();
 }
 function animatePress(currentColour) {
